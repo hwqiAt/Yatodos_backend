718199b76d571c7879c5783f988ec6b7ffa12581
huy <lengochuyjob@gmai.com>
2025-10-23 00:05:18 +0700

    update cors to test local

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import cors from "cors";
+import cors, { CorsOptions } from "cors";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth";
 import todoRoutes from "./routes/todos";
@@ -10,9 +10,26 @@ const app = express();
 
 app.use(express.json());
 
-const corsOptions = {
-  origin: "https://yatodos-web-git-main-hwqiats-projects.vercel.app",
+const allowedOrigins = [
+  "https://yatodos-web-git-main-hwqiats-projects.vercel.app",
+  "http://localhost:3000",
+];
+type CorsCallback = (err: Error | null, allow?: boolean) => void;
+
+const corsOptions: CorsOptions = {
+  origin: function (
+    origin: string | undefined, // Thêm kiểu cho origin
+    callback: CorsCallback // Sử dụng kiểu đã định nghĩa
+  ) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
 };
+
 app.use(cors(corsOptions));
 
 app.use("/api/auth", authRoutes);
